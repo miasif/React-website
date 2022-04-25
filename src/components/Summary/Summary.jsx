4b11f83b8d1d8d5cc938d57cacf2999c05d235b2
miasif <minhazul.islam.asif@gmail.com>
2022-04-25 14:02:06 +0600

    summary start

---
+++ src/components/Summary/Summary.jsx
@@ -0,0 +1,46 @@
+import React, { Component, Fragment } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+
+class Summary extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Container fluid={true} className="summaryBanner p-0">
+          <div className="summaryBannerOverlay">
+            <Container className="text-center">
+              <Row>
+                <Col lg={8} md={6} sm={12}>
+                  <Row className="countSection">
+                    <Col>
+                      <h1 className="countNumber">35000 </h1>
+                      <h4 className="countTitle">Students Worldwide</h4>
+                      <hr className="bg-white w-25" />
+                    </Col>
+
+                    <Col>
+                      <h1 className="countNumber">22</h1>
+                      <h4 className="countTitle">Courses Published</h4>
+                      <hr className="bg-white w-25" />
+                    </Col>
+
+                    <Col>
+                      <h1 className="countNumber">3000</h1>
+                      <h4 className="countTitle">Student Reviews</h4>
+                      <hr className="bg-white w-25" />
+                    </Col>
+                  </Row>
+                </Col>
+
+                <Col lg={4} md={6} sm={12}>
+                  <h1>FOUR </h1>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </Container>
+      </Fragment>
+    );
+  }
+}
+
+export default Summary;
